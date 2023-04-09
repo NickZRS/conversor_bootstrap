52f03ce3d178812101c7b190f95f124e0b9a28e7
NickZRS <nick1804js@gmail.com>
2023-04-09 17:42:18 -0300

    conversor_02

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class ConversorComponent {
 
+    metro: number = 0;
+    resp: string = "";
+
+    milimetro(){
+       
+      (<HTMLInputElement>document.getElementById("resp")).value = (this.metro*1000)+" Mílimetros"
+    }
+
+    centimetro(){
+      
+      (<HTMLInputElement>document.getElementById("resp")).value = (this.metro*100)+" Centímetros"
+    }
+    
+
+    quilometro(){
+
+      (<HTMLInputElement>document.getElementById("resp")).value = (this.metro/1000)+" Quilômetros"
+    }
+    
+
+
 }
